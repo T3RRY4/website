ac4271a31d98ff064016b55809645f0a950b2294
T3RRY4 <vladik.nekrasov3@gmail.com>
2022-07-17 16:22:13 +0400

    Replaced local icons with '@indaco/svelte-iconoir' package

--- src/routes/_footer.svelte
+++ src/routes/_footer.svelte
@@ -1,15 +1,15 @@
 <script>
-  import Telegram from "../icons/_telegram.svelte";
-  import Discord from "../icons/_discord.svelte";
-  import Github from "../icons/_github.svelte";
-  import Mail from "../icons/_mail.svelte";
+  import TelegramIcon from '@indaco/svelte-iconoir/icons/TelegramIcon.svelte';
+  import DiscordIcon from '@indaco/svelte-iconoir/icons/DiscordIcon.svelte';
+  import GitHubIcon from '@indaco/svelte-iconoir/icons/GitHubIcon.svelte';
+  import MailIcon from '@indaco/svelte-iconoir/icons/MailIcon.svelte';
 </script>
 
 <footer>
   <nav>
-    <a href="https://t.me/t3rry4"><Telegram /></a>
-    <a href="https://discord.gg/FS5VyCgYxK"><Discord /></a>
-    <a href="https://github.com/T3RRY4"><Github /></a>
-    <a href="mailto:imterry4@hotmail.com"><Mail /></a>
+    <a href="https://t.me/t3rry4"><TelegramIcon /></a>
+    <a href="https://discord.gg/FS5VyCgYxK"><DiscordIcon /></a>
+    <a href="https://github.com/T3RRY4"><GitHubIcon /></a>
+    <a href="mailto:imterry4@hotmail.com"><MailIcon /></a>
   </nav>
 </footer>
